d35d638c214bc946850178d928093797e58e7be8
James Ward <james@notjam.es>
2023-11-01 20:00:10 -0400

    test: add a basic queue test

--- tests/queue_test.py
+++ tests/queue_test.py
@@ -1,8 +1,13 @@
-from asyncio import Queue
+# SPDX-FileCopyrightText: 2019-2020 Damien P. George
+#
+# SPDX-License-Identifier: MIT
+#
+# MicroPython uasyncio module
+# MIT license; Copyright (c) 2019-2020 Damien P. George
+
+from asyncio import Queue, run as run_task
 import sys
 
-
-print(adafruit_asyncio)
 
 def test_single_queue():
     async def run_single_queue():
@@ -16,8 +21,8 @@ def test_single_queue():
 
         assert await queue.get() == ("Hello world", 0)
         assert await queue.get() == ("Hello world", 1)
-        assert await queue.get() == ("Hello world", 1)
+        assert await queue.get() == ("Hello world", 2)
         assert await queue.get() == ("Hello world", 3)
         assert await queue.get() == ("Hello world", 4)
 
-    adafruit_asyncio.run(run_single_queue())
+    run_task(run_single_queue())
